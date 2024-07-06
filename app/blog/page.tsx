933392103a99541fda89cc85566d43cd32cbf4e1
Arindam Majumder <arindammajumder2020@gmail.com>
2024-07-06 22:56:15 +0530

    chore: Update blog page metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,23 @@
 import { BlogPosts } from "app/components/posts";
+import type { Metadata } from "next";
+import { baseUrl } from "../sitemap";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Blog",
   description: "Read my blog.",
+  openGraph: {
+    title: "Blog | Arindam",
+    description: "Blogs of Arindam Majumder.",
+    url: baseUrl + "/blog",
+    siteName: "Arindam Majumder",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    title: 'Blog | Arindam',
+    card: 'summary_large_image',
+    description: "Blogs of Arindam Majumder.",
+  },
 };
 
 export default function Page() {
