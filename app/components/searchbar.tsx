3877e6b539aa33910d03a741e1a65ff852f43217
Arindam Majumder <arindammajumder2020@gmail.com>
2024-07-21 17:36:39 +0530

    fix: error handling

--- app/components/searchbar.tsx
+++ app/components/searchbar.tsx
@@ -32,7 +32,6 @@ function Searchbar() {
         className="border md:w-11/12 rounded border-neutral-200 bg-neutral-50 px-3 py-1.5 dark:border-neutral-700 dark:bg-neutral-800"
         placeholder="Search My Blogs"
         defaultValue={searchParams.get("query")?.toString()}
-
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
--- app/error.tsx
+++ app/error.tsx
@@ -10,13 +10,16 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <p>Something went wrong...Try to refresh and see if it stays or not?</p>
+      <button
+        className="justify-center py-2 border cursor-pointer border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100"
+        onClick={() => reset()}
+      />
     </div>
   );
 }
