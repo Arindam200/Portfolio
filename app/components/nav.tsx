94a7ad8f05db46a954e69695594eedf9b95f7558
Arindam Majumder <arindammajumder2020@gmail.com>
2024-08-12 11:35:39 +0530

    refactor: Update nav component imports and formatting

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,8 +1,8 @@
-'use client'
- 
-import { usePathname } from 'next/navigation'
+"use client";
+
+import { usePathname } from "next/navigation";
 import { Link } from "next-view-transitions";
-import clsx from 'clsx';
+import clsx from "clsx";
 
 const navItems = {
   "/": {
@@ -24,7 +24,7 @@ const navItems = {
 };
 
 export function Navbar() {
-  const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <aside className="-ml-[8px] mb-14 md:mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
@@ -38,9 +38,13 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className={clsx("transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-0.75 sm:px-1 md:px-2 m-1",{
-                    "underline underline-offset-4 transition-all transform-stroke": pathname === path,
-                  })}
+                  className={clsx(
+                    "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-0.75 sm:px-1 md:px-2 m-1",
+                    {
+                      "underline underline-offset-4 transition-all transform-stroke":
+                        pathname === path,
+                    },
+                  )}
                 >
                   {name}
                 </Link>
