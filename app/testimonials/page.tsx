5bdcad948504c2629f1c7dc2f9b57901fe3dc96b
Arindam Majumder <arindammajumder2020@gmail.com>
2024-08-16 00:14:38 +0530

    feat: add testimonial highlight

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -12,6 +12,7 @@ import David from "./assets/David.png";
 import { ArrowIcon } from "../components/icons";
 import { Link } from "next-view-transitions";
 import type { Metadata } from "next";
+import { highlight } from "sugar-high";
 
 export const metadata: Metadata = {
   title: "Testimonials",
@@ -24,12 +25,14 @@ function TestimonialCard({
   link,
   AuthorDescription,
   content,
+  highlights = [],
 }: {
   image: any;
   name: string;
   link: string;
   AuthorDescription: string;
   content: string;
+  highlights?: string[];
 }): React.JSX.Element {
   return (
     <div className="">
@@ -58,7 +61,33 @@ function TestimonialCard({
             </div>
           </Link>
         </div>
-        <p className="text-foreground-muted text-base pl-2 mt-2">{content}</p>
+        <div className="text-foreground-muted text-base pl-2 mt-2">
+          {content.split("\n").map((line, index) => (
+            <p key={index} className="mb-2">
+              {highlights.reduce(
+                (acc, highlight) =>
+                  acc.flatMap((part, i) =>
+                    typeof part === "string" && part.includes(highlight)
+                      ? part.split(highlight).flatMap((subPart, j) =>
+                          j < part.split(highlight).length - 1
+                            ? [
+                                subPart,
+                                <span
+                                  key={`${highlight}-${j}`}
+                                  className="text-green-800 dark:text-green-300"
+                                >
+                                  {highlight}
+                                </span>,
+                              ]
+                            : [subPart],
+                        )
+                      : [part],
+                  ),
+                [line],
+              )}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -71,78 +100,126 @@ export default function page() {
         My Testimonials
       </h1>
       <p className="">Here's what people are saying about me:</p>
+
       <TestimonialCard
         image={Gabriel}
         name="Gabriel L. Manor"
         link="https://testimonial.to/arindam-s-blog/all"
         AuthorDescription="Director of DevRel, Permit.io"
-        content="We've been working with Arindam on multiple content pieces for the last couple of months, and his work is astonishing. One of his strengths is his ability and willingness to learn and improve. He never gave up on this and made sure the content was always accurate and exact to the required point. We also love his network of writers, where he can scale the workload when needed. So far, 10/10 experience!"
+        content={`We've been working with Arindam on multiple content pieces for the last couple of months, and his work is astonishing. 
+          One of his strengths is his ability and willingness to learn and improve. He never gave up on this and made sure the content was always accurate and exact to the required point.
+          We also love his network of writers, where he can scale the workload when needed. 
+          
+          So far, 10/10 experience!`}
+        highlights={[
+          "his work is astonishing",
+          "his ability and willingness to learn and improve",
+          "We also love his network of writers",
+          "So far, 10/10 experience!",
+        ]}
       />
       <TestimonialCard
         image={Julia}
         name="Julia Machado"
         link="https://testimonial.to/arindam-s-blog/all"
         AuthorDescription="Founder & CEO, Webcrumbs"
-        content="Arindam gave us a masterclass on how to launch on Product Hunt and I dare to say if it wasn't that we wouldn't have achieved 3rd in our first launch ever and with almost zero preparation. He's also a super nice guy, and really smart. Write great technical texts that reach an impressive audience. We'll definitely work with him again!"
+        content={`Arindam gave us a masterclass on how to launch on Product Hunt and I dare to say if it wasn't that we wouldn't have achieved 3rd in our first launch ever and with almost zero preparation. 
+          
+          He's also a super nice guy, and really smart. Write great technical texts that reach an impressive audience.
+          
+          We'll definitely work with him again!`}
+        highlights={[
+          "Arindam gave us a masterclass on how to launch on Product Hunt",
+          "3rd in our first launch ever",
+          "super nice guy",
+          "Write great technical texts",
+          "We'll definitely work with him again!",
+        ]}
       />
       <TestimonialCard
         image={Alexandr}
         name="Alexandr"
         link="https://testimonial.to/arindam-s-blog/all"
         AuthorDescription="Lead at OneEntry Portal Co"
-        content="I had the pleasure of working with Arindam on a article for our company. From our initial conversation to the final submission, Arindam's professionalism and expertise, for their craft were evident. Communication was seamless and enjoyable. He fully understood the nuances of the project. The entire collaboration process not only productive but genuinely pleasant. The article was not only informative and engaging but also aligned with our vision."
+        content={`I had the pleasure of working with Arindam on a article for our company. From our initial conversation to the final submission, Arindam's professionalism and expertise, for their craft were evident. Communication was seamless and enjoyable. 
+          He fully understood the nuances of the project. The entire collaboration process not only productive but genuinely pleasant. 
+          The article was not only informative and engaging but also aligned with our vision.`}
+        highlights={[
+          "Arindam's professionalism and expertise",
+          "Communication was seamless and enjoyable",
+          "The entire collaboration process not only productive but genuinely pleasant",
+          "informative and engaging",
+          "aligned with our vision.",
+        ]}
       />
       <TestimonialCard
         image={David}
         name="David Mytton"
         link="https://davidmytton.blog/"
         AuthorDescription="CEO of Arcjet"
-        content="We worked with Arindam on a writeup of the Arcjet beta release. The goal was to introduce the SDK to developers and show off how you can use Arcjet to protect an interesting application. Arindam was responsive to feedback and helped us achieve those goals"
-      />
-      <TestimonialCard
-        image={Eddie}
-        name="Eddie Jaoude"
-        link="https://x.com/eddiejaoude"
-        AuthorDescription="GitHub Star & DevRel"
-        content="Arindam is a great DevRel and is always super supportive to everyone in the community, his content is insightful and I always enjoy reading his blogs"
-      />
-      <TestimonialCard
-        image={Sophia}
-        link="https://x.com/sophiairoegbu_"
-        name="Sophia Iroegbu"
-        AuthorDescription="Dev Advocate at LeadDevRel"
-        content="Arindam is an amazing friend and colleague, we worked on some campaigns while at Pieces and Lead DevRel. 
-        Also, he helped me improve my writing skills as he gave me feedback on some articles I wrote.."
-      />
-      <TestimonialCard
-        image={Debajyati}
-        link="https://x.com/ddebajyati/status/1762896523963326884"
-        name="Debajyati Dey"
-        AuthorDescription="@ddebajyati"
-        content="You're a gift to the community. You were the one who inspired me to write blogs. Your blogs are awesome 🚀. 
-        You deserve appreciation. 😊🤓"
+        content={`We worked with Arindam on a writeup of the Arcjet beta release. The goal was to introduce the SDK to developers and show off how you can use Arcjet to protect an interesting application. 
+          Arindam was responsive to feedback and helped us achieve those goals`}
+        highlights={["Arindam was responsive", "helped us achieve those goals"]}
       />
       <TestimonialCard
         link="https://x.com/Arindam_1729/status/1762092650780578240"
         image={Dani}
         name="Dani Passos"
         AuthorDescription="DevRel at Shopify"
-        content="@Arindam_1729 That is amazing, I love reading your articles 🔥"
+        content={`Working with Ari has been an absolute pleasure and a truly enriching experience. His passion shines through in the enthusiasm he brings to each task, no matter how big or small. 
+
+          What sets him apart is his exceptional attention to detail; he always ensures that every aspect of his work is polished to perfection.
+
+          Ari consistently goes above and beyond what is expected of him, setting a new standard for professionalism and commitment. His proactive approach means that he not only meets deadlines but often exceeds expectations, delivering results that are truly impressive. 
+          
+          Managing Ari at Lead DevRel has been inspiring, as his drive and determination motivate those around him to elevate their own performance. His dedication, combined with his meticulous nature and willingness to go the extra mile, makes Ari an invaluable asset to any team or project he's involved with.`}
+        highlights={[
+          "absolute pleasure and a truly enriching experience",
+          "enthusiasm",
+          "exceptional attention to detail",
+          "polished to perfection",
+          "goes above and beyond",
+          "often exceeds expectations",
+          "Managing Ari at Lead DevRel has been inspiring",
+          "dedication, combined with his meticulous nature and willingness to go the extra mile",
+        ]}
       />
       <TestimonialCard
-        image={Suhas}
-        link="https://x.com/Arindam_1729/status/1762092650780578240"
-        name="Suhas Sumukh"
-        AuthorDescription="@suhasasumukh"
-        content="@Arindam_1729 writes awesome blogs on Hashnode"
+        image={Eddie}
+        name="Eddie Jaoude"
+        link="https://x.com/eddiejaoude"
+        AuthorDescription="GitHub Star & DevRel"
+        content={`Arindam is a great DevRel and is always super supportive to everyone in the community, his content is insightful and I always enjoy reading his blogs`}
+        highlights={[
+          "great DevRel",
+          "super supportive",
+          "content is insightful",
+        ]}
       />
+      <TestimonialCard
+        image={Sophia}
+        name="Sophia Iroegbu"
+        link="https://x.com/sophiairoegbu_"
+        AuthorDescription="Dev Advocate at LeadDevRel"
+        content={`Arindam is an amazing friend and colleague. 
+          We worked on some campaigns while at Pieces and Lead DevRel. 
+          Also, he helped me improve my writing skills as he gave me feedback on some articles I wrote.`}
+        highlights={[
+          "Arindam is an amazing friend",
+          "improve my writing skills",
+        ]}
+      />
+
       <TestimonialCard
         image={Debajyati}
-        link="https://x.com/Arindam_1729/status/1762092650780578240"
+        link="https://x.com/ddebajyati/status/1762896523963326884"
         name="Debajyati Dey"
         AuthorDescription="@ddebajyati"
-        content="You're a great blogger brother. 🙌🏻
-        What I admire most is that you're highly consistent!"
+        content={`You're a gift to the community. 
+          You were the one who inspired me to write blogs.
+          Your blogs are awesome 🚀. 
+          You deserve appreciation. 😊🤓`}
+        highlights={["inspired me", "Your blogs are awesome"]}
       />
     </div>
   );
