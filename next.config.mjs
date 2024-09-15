6d94e01a4d5b4da9dff1e2cf805152ca643ac823
Arindam Majumder <arindammajumder2020@gmail.com>
2024-09-15 10:36:38 +0530

    blurred img

--- next.config.mjs
+++ next.config.mjs
@@ -12,13 +12,11 @@ const config = {
         protocol: "https",
         hostname: "cdn.hashnode.com",
         port: "",
-        //   pathname: '/account123/**',
       },
       {
         protocol: "https",
         hostname: "dev-to-uploads.s3.amazonaws.com",
         port: "",
-        //   pathname: '/**',
       },
     ],
   },
